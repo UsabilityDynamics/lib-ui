ca21b166e1df49faac285da172d735a0db917a0e
Andy Potanin <andy.potanin@usabilitydynamics.com>
2013-09-07 19:54:43 -0400

    first commit

--- index.js
+++ index.js
@@ -3,4 +3,4 @@
  *
  * @author potanin@UD
  */
-module.exports = process.env.APP_COVERAGE ? require( './static/codex/lib-cov/{{NAME}}' ) : require( './scripts/{{NAME}}' );
+module.exports = process.env.APP_COVERAGE ? require( './static/codex/lib-cov/interface' ) : require( './scripts/interface' );
