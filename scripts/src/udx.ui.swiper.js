c8d5205c225b4511ae3f7eaeeee50b9f34ff4ceb
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-02-06 02:35:37 -0500

    Automated commit.

---
+++ scripts/src/udx.ui.swiper.js
@@ -0,0 +1,16 @@
+/**
+ * Swiper
+ *
+ */
+define( 'udx.ui.swiper', [ 'udx.utility' ], function Swiper() {
+  console.debug( 'udx.ui.swiper', 'loaded' );
+
+  return function domnReady() {
+    console.debug( 'udx.ui.swiper', 'ready' );
+
+    return this;
+
+  };
+
+});
+
