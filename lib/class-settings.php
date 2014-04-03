5f0a86883a0c35c1a5040af500015e3d3798c99b
Maxim Peshkov <maxim.peshkov@usabilitydynamics.com>
2014-04-03 14:43:38 +0300

    added class settings (it will handle edit settings pages)

---
+++ lib/class-settings.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Settings User Interface
+ *
+ * @author peshkov@UD
+ */
+namespace UsabilityDynamics\UI {
+
+  if( !class_exists( 'UsabilityDynamics\UI\Settings' ) ) {
+
+    /**
+     * Class Settings
+     *
+     */
+    class Settings {
+
+      /**
+       * Constructor
+       *
+       */
+      public function __construct( $args = false ) {
+        
+        $args = wp_parse_args( $args, array(
+          'schema' => array(),
+        ) );
+        
+      }
+    
+    }
+
+  }
+
+}
