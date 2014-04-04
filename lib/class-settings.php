eec6a63c035a32a98994f00cf69c59b2f25955d8
Maxim Peshkov <maxim.peshkov@usabilitydynamics.com>
2014-04-04 20:17:43 +0300

    wip

--- lib/class-settings.php
+++ lib/class-settings.php
@@ -17,15 +17,70 @@ namespace UsabilityDynamics\UI {
       /**
        * Constructor
        *
+       * @param UsabilityDynamics\Settings object $settings
+       * @param array $args
        */
       public function __construct( $settings, $args = false ) {
-        
-        //is_subclass_of 
         
         $args = wp_parse_args( $args, array(
           'schema' => array(),
         ) );
         
+        //** Break if settings var is incorrect */
+        if( !is_subclass_of( $settings, 'UsabilityDynamics\Settings' ) ) {
+          return;
+        }
+        //** Break if schema is not valid */
+        if( !$this->parse_schema( $args[ 'schema' ] ) ) {
+          return;
+        }
+        
+        //** Now initialize settings UI. */
+        
+      }
+      
+      /**
+       * Parse schema:
+       * - Validates schema
+       * - Adds Settings Menu
+       *
+       */
+      private function parse_schema( $schema = array() ) {
+        
+        //echo "<pre>"; print_r( $schema ); echo "</pre>"; die();
+        
+        try {
+        
+          if( empty( $schema[ 'configuration' ] ) ) {
+            throw new Exception( 'configuration data is not set' );
+          } else if ( empty( $schema[ 'fields' ] ) ) {
+            throw new Exception( 'fields data is not set' );
+          }
+          
+          if( !$this->add_menu( $schema[ 'configuration' ] ) ) {
+            throw new Exception( 'menu could not be added' );
+          }
+        
+        } catch ( Exception $e ) {
+          // @todo: log to console (firebug)
+          return false;
+        }
+        
+        return true;
+        
+      }
+      
+      /**
+       *
+       *
+       */
+      private function add_menu( $config ) {
+        
+        $config = wp_parse_args( $config, array(
+          'menu' => false,
+        ) );
+        
+        return false;
       }
     
     }
