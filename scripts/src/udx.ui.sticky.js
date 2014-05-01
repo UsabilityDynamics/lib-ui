9f2b0dd7355db9f408a9ea10353d0ca79b932926
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-05-01 01:39:18 -0400

    wip

--- scripts/src/udx.ui.sticky.js
+++ scripts/src/udx.ui.sticky.js
@@ -16,33 +16,173 @@
  *
  * @example
  *
- *    <div data-requires="udx.ui.sticky-header" data-class-cloned="is-cloned" class="nav"></div>
+ *    <div data-requires="udx.ui.sticky" data-class-cloned="is-cloned" class="nav"></div>
  *
- * @example http://www.onlywebpro.com/2013/04/03/make-a-jquery-sticky-header-in-5-minutes/
  * @source http://jsfiddle.net/XyVAG/9/
  */
 define( 'udx.ui.sticky', [ 'jquery' ], function stickyHeader() {
-  console.debug( 'udx.ui.sticky-header', 'Loaded.' );
+  console.debug( 'udx.ui.sticky', 'Loaded.' );
 
-  return function domnReady() {
-    console.debug( 'udx.ui.sticky-header', 'Initialized.' );
+  console.debug( typeof jQuery );
+  console.debug( typeof $ );
 
-    // Get Options.
-    var options = {
-      cloned: this.getAttribute( 'data-class-cloned' ) || 'udx-cloned',
-      down: this.getAttribute( 'data-class-down' ) || 'udx-down'
+  // Get Options.
+  var options = {
+    location: this.getAttribute( 'data-option-location' ) || 'header',
+    cloned: this.getAttribute( 'data-option-class-cloned' ) || 'udx-cloned',
+    down: this.getAttribute( 'data-option-class-down' ) || 'udx-down'
+  };
+
+  var defaults = {
+      topSpacing: 0,
+      bottomSpacing: 0,
+      className: 'is-sticky',
+      wrapperClassName: 'sticky-wrapper',
+      center: false,
+      getWidthFrom: ''
+    },
+    $window = $(window),
+    $document = $(document),
+    sticked = [],
+    windowHeight = $window.height(),
+    scroller = function() {
+      var scrollTop = $window.scrollTop(),
+        documentHeight = $document.height(),
+        dwh = documentHeight - windowHeight,
+        extra = (scrollTop > dwh) ? dwh - scrollTop : 0;
+
+      for (var i = 0; i < sticked.length; i++) {
+        var s = sticked[i],
+          elementTop = s.stickyWrapper.offset().top,
+          etse = elementTop - s.topSpacing - extra;
+
+        if (scrollTop <= etse) {
+          if (s.currentTop !== null) {
+            s.stickyElement
+              .css('position', '')
+              .css('top', '');
+            s.stickyElement.parent().removeClass(s.className);
+            s.currentTop = null;
+          }
+        }
+        else {
+          var newTop = documentHeight - s.stickyElement.outerHeight()
+                         - s.topSpacing - s.bottomSpacing - scrollTop - extra;
+          if (newTop < 0) {
+            newTop = newTop + s.topSpacing;
+          } else {
+            newTop = s.topSpacing;
+          }
+          if (s.currentTop != newTop) {
+            s.stickyElement
+              .css('position', 'fixed')
+              .css('top', newTop);
+
+            if (typeof s.getWidthFrom !== 'undefined') {
+              s.stickyElement.css('width', $(s.getWidthFrom).width());
+            }
+
+            s.stickyElement.parent().addClass(s.className);
+            s.currentTop = newTop;
+          }
+        }
+      }
+    },
+    resizer = function() {
+      windowHeight = $window.height();
+    },
+    methods = {
+      init: function(options) {
+        var o = $.extend(defaults, options);
+        return this.each(function() {
+          var stickyElement = $(this);
+
+          var stickyId = stickyElement.attr('id');
+          var wrapper = $('<div></div>')
+            .attr('id', stickyId + '-sticky-wrapper')
+            .addClass(o.wrapperClassName);
+          stickyElement.wrapAll(wrapper);
+
+          if (o.center) {
+            stickyElement.parent().css({width:stickyElement.outerWidth(),marginLeft:"auto",marginRight:"auto"});
+          }
+
+          if (stickyElement.css("float") == "right") {
+            stickyElement.css({"float":"none"}).parent().css({"float":"right"});
+          }
+
+          var stickyWrapper = stickyElement.parent();
+          stickyWrapper.css('height', stickyElement.outerHeight());
+          sticked.push({
+            topSpacing: o.topSpacing,
+            bottomSpacing: o.bottomSpacing,
+            stickyElement: stickyElement,
+            currentTop: null,
+            stickyWrapper: stickyWrapper,
+            className: o.className,
+            getWidthFrom: o.getWidthFrom
+          });
+        });
+      },
+      update: scroller,
+      unstick: function(options) {
+        return this.each(function() {
+          var unstickyElement = $(this);
+
+          removeIdx = -1;
+          for (var i = 0; i < sticked.length; i++)
+          {
+            if (sticked[i].stickyElement.get(0) == unstickyElement.get(0))
+            {
+              removeIdx = i;
+            }
+          }
+          if(removeIdx != -1)
+          {
+            sticked.splice(removeIdx,1);
+            unstickyElement.unwrap();
+            unstickyElement.removeAttr('style');
+          }
+        });
+      }
     };
 
-    var _header = jQuery( this );
-    var _clone  = _header.before( _header.clone().addClass( options.cloned ) );
-    var _window = jQuery( window );
-    var _body   = jQuery( 'body' );
+  // should be more efficient than using $window.scroll(scroller) and $window.resize(resizer):
+  if (window.addEventListener) {
+    window.addEventListener('scroll', scroller, false);
+    window.addEventListener('resize', resizer, false);
+  } else if (window.attachEvent) {
+    window.attachEvent('onscroll', scroller);
+    window.attachEvent('onresize', resizer);
+  }
 
-    _window.on( 'scroll', function() {
-      _body.toggleClass( options.cloned, ( _window.scrollTop() > 200 ) );
-    });
+  $.fn.sticky = function(method) {
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if (typeof method === 'object' || !method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error('Method ' + method + ' does not exist on jQuery.sticky');
+    }
+  };
 
-    return this;
+  $.fn.unstick = function(method) {
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if (typeof method === 'object' || !method ) {
+      return methods.unstick.apply( this, arguments );
+    } else {
+      $.error('Method ' + method + ' does not exist on jQuery.sticky');
+    }
+
+  };
+
+  return function domnReady() {
+    console.debug( 'udx.ui.sticky', 'Initialized.' );
+
+    $( this ).sticky({topSpacing:0});
+
+    console.log( this );
 
   };
 
