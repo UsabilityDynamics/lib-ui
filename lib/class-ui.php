52f885a49d54454d4caec21328127e99713feb61
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-05-08 19:45:15 -0400

    dir structure change to std. added shell add_post_option and add_plugin_page_tab methods. added base UI class

---
+++ lib/class-ui.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Main UI Class.
+ *
+ * @author potanin@UD
+ */
+namespace UsabilityDynamics {
+
+  if( !class_exists( 'UsabilityDynamics\UI' ) ) {
+
+    /**
+     * Class UI
+     *
+     */
+    class UI {
+
+      static public function add_post_option() {}
+
+      static public function add_plugin_page_tab() {}
+
+    }
+
+  }
+
+}
