1d763e9390af147d81bc2e00e045f91bbd90c5f1
pemaxim <maxim.peshkov@usabilitydynamics.com>
2014-09-24 20:20:44 +0300

    fixes

--- lib/class-settings.php
+++ lib/class-settings.php
@@ -38,6 +38,8 @@ namespace UsabilityDynamics\UI {
        */
       public function __construct( $settings = null, $schema = null ) {
         
+        //echo "<pre>"; print_r( $schema ); echo "</pre>"; die();
+        
         //** Break if settings var is incorrect */
         if( $settings && !is_subclass_of( $settings, 'UsabilityDynamics\Settings' ) ) {
 
@@ -82,7 +84,7 @@ namespace UsabilityDynamics\UI {
         $capability = 'manage_options';
         
         // Maybe add main menu
-        if ( $main_menu && is_string( $main_menu ) ) {
+        if ( isset( $main_menu ) && is_string( $main_menu ) ) {
           if ( !isset( $submenu[ $main_menu ] ) ) {
             // Menu must exists if we pass the string.
             return false;
@@ -95,7 +97,7 @@ namespace UsabilityDynamics\UI {
               break;
             }
           }
-        }  elseif ( $main_menu && is_array( $main_menu ) ) {
+        }  elseif ( isset( $main_menu ) && is_array( $main_menu ) ) {
           extract( $main_menu = wp_parse_args( $main_menu, array(
             'page_title' => '',
             'menu_title' => '',
@@ -106,12 +108,10 @@ namespace UsabilityDynamics\UI {
           ) ) );
           add_menu_page( $page_title, $menu_title, $capability, $menu_slug, array( $this, 'render' ), $icon_url, $position );
           $parent_slug = $menu_slug;
-        } else {
-          return false;
         }
         
         //Maybe add secondary menu
-        if ( $main_menu && is_array( $secondary_menu ) && isset( $parent_slug ) ) {
+        if ( is_array( $secondary_menu ) ) {
           extract( $secondary_menu = wp_parse_args( $secondary_menu, array(
             'parent_slug' => $parent_slug,
             'page_title' => '',
@@ -119,8 +119,10 @@ namespace UsabilityDynamics\UI {
             'capability' => $capability,
             'menu_slug' => '',
           ) ) );
-          $id = add_submenu_page( $parent_slug, $page_title, $menu_title, $capability, $menu_slug, array( $this, 'render' ) );
-          add_action( 'load-' . $id, array( $this, 'request' ) );
+          if( isset( $parent_slug ) ) {
+            $id = add_submenu_page( $parent_slug, $page_title, $menu_title, $capability, $menu_slug, array( $this, 'render' ) );
+            add_action( 'load-' . $id, array( $this, 'request' ) );
+          }
         }
         
       }
