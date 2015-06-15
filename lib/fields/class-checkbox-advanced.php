ce94ec2bcd572f2bc328e13c35da9a9f7ebba837
pemaxim <maxim.peshkov@usabilitydynamics.com>
2015-06-15 18:55:20 +0300

    added Post field (post)

--- lib/fields/class-checkbox-advanced.php
+++ lib/fields/class-checkbox-advanced.php
@@ -15,15 +15,13 @@ namespace UsabilityDynamics\UI {
        * @return string
        */
       static public function html($meta, $field) {
-        $desc = $field->desc ? "<span id='{$field->id}_description' class='description'>{$field->desc}</span>" : '';
         return sprintf(
-          '<label><input type="hidden" name="%s" value="0"><input type="checkbox" class="uisf-checkbox" data-extra="%s" name="%s" id="%s" value="1" %s> %s</label>',
+          '<label><input type="hidden" name="%s" value="0"><input type="checkbox" class="uisf-checkbox" data-extra="%s" name="%s" id="%s" value="1" %s></label>',
           $field->field_name,
           $field->extra,
           $field->field_name,
           $field->id,
-          checked(!empty($meta), 1, false),
-          $desc
+          checked(!empty($meta), 1, false)
         );
       }
 
